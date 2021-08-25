f9c5a464342b77f1d09933eef6f5649231332e40
gunayozcan <61664535+gunayozcan@users.noreply.github.com>
2021-08-25 17:36:18 +0300

    SignalR client and Server Add

---
+++ SignalRServer/SignalRServer/Hubs/MyHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            //reciveMessage fonksiyonun adı
+            await Clients.All.SendAsync("reciveMessage", message);
+        }
+    }
+}
